764b396a4cd6e6c4e7c3c3d214eb6c2aa2719b03
manas140 <manasbhosale.14@gmail.com>
2022-01-20 07:56:38 +0000

    yea

---
+++ script.js
@@ -0,0 +1,27 @@
+var r = document.querySelector(':root');
+const button = document.querySelector(".toggle-btn");
+button.addEventListener("click", toggle);
+
+if (localStorage.getItem("bg") === null && localStorage.getItem("fg") === null) {
+  bg = rs.getPropertyValue('--bg');
+  fg = rs.getPropertyValue('--fg');
+} else {
+  bg = localStorage.getItem("bg");
+  fg = localStorage.getItem("fg");
+  load();
+}
+
+function toggle() {
+  var rs = getComputedStyle(r);
+  bg = rs.getPropertyValue('--bg');
+  fg = rs.getPropertyValue('--fg');
+  r.style.setProperty('--bg', fg);
+  r.style.setProperty('--fg', bg);
+  localStorage.setItem("bg", fg);
+  localStorage.setItem("fg", bg);
+}
+
+function load() {
+  r.style.setProperty('--bg', bg);
+  r.style.setProperty('--fg', fg);
+}
