20403648c5a22a12e50827856ec187801dc074cb
manas140 <manasbhosale.14@gmail.com>
2022-07-20 15:03:03 +0000

    new design

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-var r = document.querySelector(':root');
-var rs = getComputedStyle(r);
-const button = document.querySelector(".toggle-btn");
-button.addEventListener("click", toggle);
+var projects = document.querySelector('.projects')
+const repos = 'https://api.github.com/users/Manas140/repos';
+const api = 'https://api.github.com/users/Manas140';
 
-if (localStorage.getItem("al") === null && localStorage.getItem("bg") === null && localStorage.getItem("fg") === null) {
-  bg = rs.getPropertyValue('--bg');
-  fg = rs.getPropertyValue('--fg');
-} else {
-  bg = localStorage.getItem("bg");
-  fg = localStorage.getItem("fg");
-  load();
-}
+fetch(api)
+  .then(res => res.json())
+  .then(data => {
+    var followers = data.followers;
+  })
 
-function toggle() {
-  var t = document.querySelector(".toggle");
-  bg = rs.getPropertyValue('--bg');
-  fg = rs.getPropertyValue('--fg');
-  r.style.setProperty('--bg', fg);
-  r.style.setProperty('--fg', bg);
-  localStorage.setItem("bg", fg);
-  localStorage.setItem("fg", bg);
-}
-
-function load() {
-  r.style.setProperty('--bg', bg);
-  r.style.setProperty('--fg', fg);
-}
+fetch(repos)
+  .then(res => res.json())
+  .then(data => {
+    i = 0;
+    num = 3;
+    while (i < num) {
+      repo=data[i]
+      projects.innerHTML += `
+        <a href="${repo.html_url}"><div> 
+          <h3>${repo.name}</h3>
+          <p>${repo.description}</p>
+          <p>Stars: ${repo.stargazers_count}</p>
+        </div></a>
+      `;
+      i++;
+    }
+  })
