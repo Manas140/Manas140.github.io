27ebc54d1879527a55b94c47f038aa554d231985
manas140 <manasbhosale.14@gmail.com>
2023-07-11 22:37:28 +0000

    design update

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-var projects = document.querySelector('.projects')
+const cards = document.querySelector('.cards')
 const repos = 'https://api.github.com/users/Manas140/repos';
-const api = 'https://api.github.com/users/Manas140'
 
 fetch(repos)
   .then(res => res.json())
   .then(data => {
-    i = 0;
-    num = 3;
-    while (i < num) {
-      repo=data[i]
-      projects.innerHTML += `
-        <a href="${repo.html_url}"><div> 
-          <h3>${repo.name}</h3>
-          <p>${repo.description}</p>
-          <p>Stars: ${repo.stargazers_count}</p>
-        </div></a>
+    data.map((d, i) => {
+      if ( d.stargazers_count < 100 ) {
+        return;
+      } 
+      cards.innerHTML += `
+        <a href="${d.html_url}"><article> 
+          <h3>${d.name}</h3>
+          <p>${d.description}</p>
+          <p><span class="material-symbols-rounded">star</span> ${d.stargazers_count}</p>
+        </article></a>
       `;
-      i++;
-    }
+    })
   })
 
 const age = document.querySelector(".age");
